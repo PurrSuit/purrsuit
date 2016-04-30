92c1bf5a17de85e7c62491b53cfb31fcbc48d3f3
Stefania <stefaniabezerra95@gmail.com>
2016-04-30 13:56:54 -0300

    validate_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true, length: { maximum: 50 }
 
+  VALID_EMAIL_REGEX	=	/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   before_save { self.email = email.downcase }
   validates :email, presence: true
-  VALID_EMAIL_REGEX	=	/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  
+
   validates :password, presence: true, length: { minimum: 8 }
 
 end
