3d2a9a3365d369e55075e6542c26879df5700cfe
Tulio Coelho <tulioopc@gmail.com>
2016-05-11 23:48:29 -0300

    morra

--- app/serializers/deputy_serializer.rb
+++ app/serializers/deputy_serializer.rb
@@ -1,3 +1,3 @@
 class DeputySerializer < ActiveModel::Serializer
-  attributes :id, :name, :age, :registration, :legislation_situation
+  attributes :id, :name,:age, :registration, :legislation_situation
 end
